bb71789d092135e4565877603f81a5b660209bbd
Kawin Sirikhanarat <kawinsirikhanarat@gmail.com>
2022-08-18 10:11:34 +0700

    add patient table

--- react/src/PatientTable.js
+++ react/src/PatientTable.js
@@ -1,5 +1,31 @@
-const PatientTable = () => {
-    
+const PatientTable = ({currentData}) => {
+    return  <TableContainer component={Paper}>
+    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+            <TableRow>
+                <TableCell>id</TableCell>
+                <TableCell>hn</TableCell>
+                <TableCell>firstname</TableCell>
+                <TableCell>lastname</TableCell>
+                <TableCell>action</TableCell>
+            </TableRow>
+        </TableHead>
+        <TableBody>
+            {currentData.map((data, index) => {
+                return <TableRow key={index}>
+                    <TableCell>{data.id}</TableCell>
+                    <TableCell>{data.hn}</TableCell>
+                    <TableCell>{data.firstname}</TableCell>
+                    <TableCell>{data.lastname}</TableCell>
+                    <TableCell>
+                        <Link to={'/patient/' + data.id}>Open</Link>
+                    </TableCell>
+                </TableRow>
+            })}
+
+        </TableBody>
+    </Table>
+</TableContainer>
 }
 
 export default PatientTable;
